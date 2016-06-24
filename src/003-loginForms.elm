fcbb484ccbe4b5f6049e3de2d90ac8b9cd36c8cd
Ignasi Marimon-Clos <ignasi35@gmail.com>
2016-06-24 20:11:23 +0200

    Adds 003 forms

---
+++ src/003-loginForms.elm
@@ -0,0 +1,51 @@
+import Html exposing (..)
+import Html.App as Html
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+main = 
+  Html.beginnerProgram { model = model, view = view, update = update }
+
+type alias Model =
+  { name : String
+  , password : String
+  , passwordAgain : String
+  }
+
+model : Model
+model = Model "" "" "" 
+
+-- UPDATE
+
+type Msg 
+  = Name String
+  | Password String
+  | PasswordAgain String
+
+update : Msg -> Model -> Model
+update msg model = 
+  case msg of 
+    Name x -> { model | name = x }
+    Password x -> { model | password = x }
+    PasswordAgain x -> { model | passwordAgain = x }
+
+-- VIEW
+
+view : Model -> Html Msg
+view model = 
+  div []
+    [ input [ type' "text", placeholder "Name", onInput Name ] []
+    , input [ type' "password", placeholder "Password", onInput Password ] []
+    , input [ type' "password", placeholder "Re-enter password", onInput PasswordAgain ] []
+    , viewValidation model
+    ]
+viewValidation : Model -> Html Msg
+viewValidation model = 
+  let
+    (color, message) = 
+      if model.password == model.passwordAgain then
+        ("green", "OK")
+      else
+        ("red", "Password do not match!")
+  in
+    div [ style [("color", color)] ] [ text message ]
