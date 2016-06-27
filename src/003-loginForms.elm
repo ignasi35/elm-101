255ab001e7175ff4ccca022f04b52cb9126e6666
Ignasi Marimon-Clos <ignasi35@gmail.com>
2016-06-27 18:07:09 +0200

    Adds extra validations on Form

--- src/003-loginForms.elm
+++ src/003-loginForms.elm
@@ -2,8 +2,9 @@ import Html exposing (..)
 import Html.App as Html
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
+import String
 
-main = 
+main =
   Html.beginnerProgram { model = model, view = view, update = update }
 
 type alias Model =
@@ -13,18 +14,19 @@ type alias Model =
   }
 
 model : Model
-model = Model "" "" "" 
+model = Model "" "" ""
+
 
 -- UPDATE
 
-type Msg 
+type Msg
   = Name String
   | Password String
   | PasswordAgain String
 
 update : Msg -> Model -> Model
-update msg model = 
-  case msg of 
+update msg model =
+  case msg of
     Name x -> { model | name = x }
     Password x -> { model | password = x }
     PasswordAgain x -> { model | passwordAgain = x }
@@ -32,20 +34,23 @@ update msg model =
 -- VIEW
 
 view : Model -> Html Msg
-view model = 
+view model =
   div []
     [ input [ type' "text", placeholder "Name", onInput Name ] []
     , input [ type' "password", placeholder "Password", onInput Password ] []
     , input [ type' "password", placeholder "Re-enter password", onInput PasswordAgain ] []
     , viewValidation model
     ]
+
 viewValidation : Model -> Html Msg
-viewValidation model = 
+viewValidation model =
   let
-    (color, message) = 
-      if model.password == model.passwordAgain then
-        ("green", "OK")
+    (color, message) =
+      if model.password /= model.passwordAgain then
+        ( "red" , "Password do not match!" )
+      else if String.length (String.trim model.password) <= 8 then
+        ( "red" , "Password too short." )
       else
-        ("red", "Password do not match!")
+        ("green", "OK")
   in
     div [ style [("color", color)] ] [ text message ]
